8898ac2fa271c14091b4c7d399ae25c2c5091639
Shedels <54005953+Shedels@users.noreply.github.com>
2020-05-13 15:36:15 +0300

    Update qurier.lua

--- Mining_v0/qurier.lua
+++ Mining_v0/qurier.lua
@@ -1,1 +1,46 @@
-local r = require("robotssssss")
+local r = require("robot") 
+
+print("Hello qurier started")
+print("Skoko w niz = ") 
+down = io.read()
+print("Dlina =") 
+dlin = io.read()
+print("Sherina")
+sher = io.read()
+
+
+local l = 0
+while l ~= down do 
+
+r.swingDown()
+r.down()
+
+for i2 = 0, i2 == sher do
+
+for i=0, i == dlin, 1 do 
+r.swing()
+r.forward()
+end
+r.tunrRight()
+r.swing()
+t.forward()
+r.tunrRight()
+for i=0, i == dlin, 1 do 
+r.swing()
+r.forward()
+end
+r.tunrLeft() 
+r.swing()
+t.forward()
+r.tunrLeft()
+
+end
+end
+
+
+
+
+
+
+
+end
